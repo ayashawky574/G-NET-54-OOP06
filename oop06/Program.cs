7b0584a21387167397abf64a651cbaaec5cbebf4
ayashawky574 <ayashawky574@gmail.com>
2026-08-19 08:15:53 +0300

    Q2

--- oop06/Program.cs
+++ oop06/Program.cs
@@ -15,6 +15,17 @@
             //abstraction :choose returant , choose food , payment method (without know how app contact with the resturant or the bank)
             // encapsultion : order status , payment data , price of order 
             #endregion
+
+            #region Q2
+            //Q2: What is the difference between an abstract class and an interface? Give at least four differences.
+            //abstract (can contain fields) interface (cant contain feilds)
+            //abstract (can contain constructor) interface (cant contain constructor)
+            //abstract (class can inherit from only one abstract) interface (class can implement many interfaces)
+            //abstract (represent is-a relation ship) interface (repersent capability)
+
+            //When would you choose one over the other?
+            //if roles or capability => interface / identity or shared behavior => abstract class
+            #endregion
             #endregion
         }
     }
