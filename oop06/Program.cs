11eeda724ffe63c4cbef50531ba6541e001560ae
ayashawky574 <ayashawky574@gmail.com>
2026-08-19 07:33:49 +0300

    Q1

--- oop06/Program.cs
+++ oop06/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            #region part01 Theoretical Questions
+            #region Q1
+            //Q1: What is abstraction in OOP?
+            //abstraction is one of oop principle which is the process of expose what user need hidding how it implemented
+            //How is it different from encapsulation?
+            // encapsultion is the process of protecting data and controll validating data
+            //Give a real - world example(not from the session) that shows the difference between the two.
+            // food delivrey App :
+            //abstraction :choose returant , choose food , payment method (without know how app contact with the resturant or the bank)
+            // encapsultion : order status , payment data , price of order 
+            #endregion
+            #endregion
         }
     }
 }
